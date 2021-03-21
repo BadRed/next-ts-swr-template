bd657b92af5a63bbc77e1b070eb6e1ad86eeeea2
Yuriy Glebov <yuriy@Yuriys-MacBook-Pro.local>
2021-03-21 19:48:33 +0000

    swr global fetcher

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { SWRConfig } from 'swr';
+import axios from 'axios';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (url) => axios.get(url).then((res) => res.data),
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
 
 export default MyApp;
