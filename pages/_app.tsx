eb2b25bb459fd844ef4ccb3c349c335ae62e2462
Yuriy Glebov <yuriy@Yuriys-MacBook-Pro.local>
2021-03-28 22:32:49 +0100

    Added viewport, title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,23 @@ import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SWRConfig } from 'swr';
 import axios from 'axios';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url) => axios.get(url).then((res) => res.data),
-      }}
-    >
-      <Component {...pageProps} />
-    </SWRConfig>
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <SWRConfig
+        value={{
+          fetcher: (url) => axios.get(url).then((res) => res.data),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </>
   );
 }
 
